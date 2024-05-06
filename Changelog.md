587c83ce4daa0e5dd7cc4f7020bd6099ccf49ffd
BartlomiejBielecki <bartlomiej.bielecki@pl.nsg.com>
2024-05-07 00:24:55 +0200

    upade weapon name and add refresh button

---
+++ Changelog.md
@@ -0,0 +1,4 @@
+## V 0.1.3
+- dodano tłumaczneie nazw broni
+- dodadno do kary NPC oraz każdej karty PRZEDMIOTU (instancja Foundy) przycisz Odśwież, po jego wciśnięciu dla nazw i opisów dla wszystkich przedmiotów
+oraz wymagania w przypadku profitów zostaną pobrane nowe dane z kompendium w przypadku włączenia języka polskiego będą to polskie nazwy, opisy, wymagania.
--- javascript/babele-register.js
+++ javascript/babele-register.js
@@ -1,9 +1,98 @@
+class compendiumrefresh {
+    static appendHeaderButton(html, fn) {
+        let isbutton = html.closest('.app').find('.refresh');
+        if (isbutton.length === 0) {
+            let openBtn = $(`<a class="refresh" title="Odświerz"><i class="fas fa-sync-alt"></i>Odświerz</a>`);
+            openBtn.click(fn);
+            html.closest('.app').find('.translate').remove();
+            let titleElement = html.closest('.app').find('.window-title');
+            openBtn.insertAfter(titleElement);
+        }
+    }
+}
+
+class falloutpl {
+  
+    static refreshNPC(document) {        
+        const aitems=document.object.items;
+        aitems.forEach(async element => {
+        if (element.type === "skill"){
+            let flag = element.flags.core;
+            if (flag !== undefined) {
+                let fullitemID = flag.sourceId;
+                const parts = fullitemID.split(".");
+                const compendiumKey = parts[1] + "." + parts[2];
+                const itemID = parts[parts.length - 1];
+                const pack = game.packs.find(p => p.collection === compendiumKey);
+                let  compendiumitem =await pack.getDocument(itemID);
+                
+                    const currentname = element.name;
+                    const compendiumname = compendiumitem.name;
+                    if (currentname !== compendiumname){
+                        element.update({"name": compendiumname})
+                    }
+                    const currentdescription = element.system.description;
+                    const compendiumdescription = compendiumitem.system.description;
+                    if (currentdescription !== compendiumdescription){
+                        element.update({"system.description":compendiumdescription})
+                    }
+                }                     
+            }
+            
+        });
+       
+    
+    }
+    static async refreshItem(document) {        
+        const aitems=document.object;
+        let flag = aitems.flags.core;
+            if (flag !== undefined) {
+                let fullitemID = flag.sourceId;
+                const parts = fullitemID.split(".");
+                const compendiumKey = parts[1] + "." + parts[2];
+                const itemID = parts[parts.length - 1];
+                const pack = game.packs.find(p => p.collection === compendiumKey);
+                let  compendiumitem =await pack.getDocument(itemID);
+                
+                    const currentname = aitems.name;
+                    const compendiumname = compendiumitem.name;
+                    if (currentname !== compendiumname){
+                        aitems.update({"name": compendiumname})
+                    }
+                    const currentdescription = aitems.system.description;
+                    const compendiumdescription = compendiumitem.system.description;
+                    if (currentdescription !== compendiumdescription){
+                        aitems.update({"system.description":compendiumdescription})
+                    }
+                    if(aitems.type === "perk"){
+                        const currentrequierment = aitems.system.requirements;
+                        const compendiumrequierment = compendiumitem.system.requirements;
+                        if (currentrequierment !== compendiumrequierment){
+                            aitems.update({"system.requirements":compendiumrequierment})
+                        }
+                    }
+
+                }  
+    }
+}
+
 Hooks.on('init', () => {
-    if(typeof Babele !== 'undefined') {
+    if (typeof Babele !== 'undefined') {
         Babele.get().register({
             module: 'fallout-pl',
             lang: 'pl',
             dir: 'compendium'
         });
     }
+});
+
+Hooks.on('renderFalloutNpcSheet', (document, html) => {
+    compendiumrefresh.appendHeaderButton(html, ev => {
+        falloutpl.refreshNPC(document);
+    });
+});
+Hooks.on('renderFalloutItemSheet', (document, html) => {
+    compendiumrefresh.appendHeaderButton(html, ev => {
+        falloutpl.refreshItem(document);
+    });
 });
